72f323f5c55b6cc96e31dd2ae4bb4f85243ac382
Jason Lewis <jlewis@harperlove.com>
2023-12-07 16:35:19 -0500

    first commit

---
+++ hangman.py
@@ -0,0 +1,24 @@
+import random
+
+def choose_movie():
+    # List of movie titles for the game
+    movies = [
+        "avatar", "titanic", "inception", "theshawshankredemption", "forrestgump",
+        "thegodfather", "pulpfiction", "thelionking", "starwars", "jurassicpark",
+        "harrypotter", "backtothefuture", "thematrix", "frozen", "braveheart",
+        "gladiator", "schindlerslist", "casablanca", "gonegirl", "interstellar",
+        "gravity", "batman", "spiderman", "superman", "avengers", "jurassicworld",
+        "blackpanther", "thor", "ironman", "captainamerica", "thefellowshipofthering",
+        "thetwotowers", "thereturnoftheking", "thegreatgatsby", "fightclub",
+        "incredibles", "findingnemo", "moulinrouge", "et", "jaws", "madmax",
+        "silenceofthelambs", "bridgetjonesdiary", "glory", "gonein60seconds",
+        "independenceday", "killbill", "twilight", "unbreakable", "zombieland",
+        "lifeofpi", "djangounchained", "whiplash", "lalaland", "ferrisbuellersdayoff",
+        "shrek", "brokebackmountain", "ragingbull", "rocky", "jumanji", "spectre",
+        "casinoroyale", "halloween", "theterminator", "hungergames", "mib",
+        "sherlockholmes", "thegrandbudapesthotel", "theexorcist", "theshining",
+        "backdraft", "reservoirdogs", "scream", "thebreakfastclub", "thefly",
+        "thefugitive", "grease", "therockyhorrorpictureshow", "jaws",
+        "thenotebook", "eclipse", "thewizardofoz", "frozen", "zootopia"
+        # Add more movie titles as needed
+    ]
